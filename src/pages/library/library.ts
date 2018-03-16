50bcded57a97314612a05e07e270dea831036abf
Yusup Saepudin <yusupsaepudin19@gmail.com>
2018-03-17 01:19:07 +0700

    update

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Quote } from '../../quote.interface';
+import { Quote } from '../../data/quote.interface';
 import quotes from '../../data/quotes';
 
 @Component({
