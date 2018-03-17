f6632135b0f6adf266721c7adf8e454acff1e538
Yusup Saepudin <yusupsaepudin19@gmail.com>
2018-03-17 11:37:54 +0700

    adding alert

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, AlertController } from 'ionic-angular';
 
 import { Quote } from '../../data/quote.interface';
 
@@ -9,11 +9,36 @@ import { Quote } from '../../data/quote.interface';
   templateUrl: 'quotes.html',
 })
 export class QuotesPage implements OnInit {
-  constructor (private navParams: NavParams) {}
+  constructor (private navParams: NavParams, private alertCtrl: AlertController) {}
   quoteGroup: {category: string, quotes: Quote[], icon: string}[];
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
+  }
+
+  onAddToFavorite(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('ok');
+          }
+        },
+        {
+          text: 'No, I changed my mind',
+          role: 'cancel',
+          handler: () => {
+            console.log('cancelled');
+          }
+        }
+
+      ]
+    });
+    alert.present();
   }
 
 }
